6f423ccd3f34eab63a5649067d0b873e7368068b
HyperRays <107126915+HyperRays@users.noreply.github.com>
2026-04-16 17:00:41 +0200

    other dataset

--- test/bench/multithread_f2s_bench.sh
+++ test/bench/multithread_f2s_bench.sh
@@ -21,8 +21,8 @@ fi
 THREAD_LIST="32 16"
 # THREAD_LIST="128 256"
 
-FAST5DIR=/mnt/nvme1/soysalm/d3_yeast_r94/fast5_files/
-OUTPUT_DIR=./run_d3_yeast_r94/f2s_thread_benchmark
+FAST5DIR=/mnt/nvme1/soysalm/d4_green_algae_r94/fast5_files/
+OUTPUT_DIR=./run_d4_green_algae_r94/f2s_thread_benchmark
 SLOW5TOOLS=./slow5tools
 
 if [ $DRY_RUN -eq 1 ]; then

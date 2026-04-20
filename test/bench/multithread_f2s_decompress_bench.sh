9c95bd8b0cd11e9f8ac478df22335672cc0f656e
HyperRays <107126915+HyperRays@users.noreply.github.com>
2026-04-20 15:04:09 +0200

    fixed thread count

--- test/bench/multithread_f2s_decompress_bench.sh
+++ test/bench/multithread_f2s_decompress_bench.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=f2s_decompress_benchmark
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=31
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=100G                 # 50G input + headroom
 #SBATCH --time=24:00:00
 #SBATCH --output=f2s_decompress_benchmark_%j.out
